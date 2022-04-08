7c3f47e494d8aff204bcdfb0c0b3c3f9099c83d4
rmcollins2 <102960950+rmcollins2@users.noreply.github.com>
2022-04-08 15:22:10 +0100

    Updated to LCM2007 and to calculate ocean cover

--- R_scripts/calc_baseline_landuses.R
+++ R_scripts/calc_baseline_landuses.R
@@ -21,6 +21,8 @@ rm(list=ls())
 library(sf)
 library(mapview)
 
+path <- "C:/Users/rmc230/OneDrive - University of Exeter/Documents/GitHub/BNG"
+
 ## (1) LOAD REQUIRED DATA
 ##     1.1 - LCM_2007 land cover map on a 2km resolution (created in the script 
 ##           'process_lcm.R')
@@ -29,12 +31,12 @@ library(mapview)
 
 ## 1.1. 2km LCM 2007
 ## -----------------
-setwd('D:/Documents/Github/BNG/Data/LCM/LCM_2km/')
+setwd(paste0(path, "/Data/LCM/LCM_2km"))
 lcm_2km <- read.csv('lcm_all_classes_2007.csv', check.names = FALSE)
 
 ## 1.2. SEER 2km grid
 ## ------------------
-setwd('D:/Documents/Github/BNG/Data/SEER_GRID/')
+setwd(paste0(path, "/Data/SEER_GRID"))
 seer_2km <- st_read('./SEER_net2km.shp')[, 'new2kid']
 
 lcm_2km <- merge(seer_2km, lcm_2km, by='new2kid')
@@ -44,36 +46,48 @@ lcm_2km <- merge(seer_2km, lcm_2km, by='new2kid')
 ## ===========================================================
 
 ## 2.1. Lcm agregation and conversion to hectares to match NEV input land uses.
-##      - Coastal: lcm classes 15 to 19
-##      - Freshwater : lcm class 14
-##      - Marine: lcm class 13
+##      - improved grass: lcm classes 4
+##      - Coastal: lcm classes 17 to 21
+##      - Freshwater : lcm class 16
+##      - Marine: lcm class 15
 ##      - Ocean: not available here (we classify as ocean the areas outside lcm
 ##        coverage but within SEER 2km along coastlines)
 ##      - Woodland: lcm classes 1 and 2
 ##      - Arable: lcm class 3 
 ##      - Improved grassland: lcm class 4
-##      - seminatural grassland: lcm classes 5 to 12 (I added also Mountain,
+##      - seminatural grassland: lcm classes 5 to 14 (I added also Mountain,
 ##        heath and bog here, which migh tnot be appropriate on a nationwide
 ##        scale, but works here as no Mountain, heath and bog exist in the NEP
 ##        area)
-##      - Urban: lcm classes 20 and 21
+##      - Urban: lcm classes 22 and 23
 ## After the conversion check that the sum of all land uses for each cell equals 
 ## 400ha
 ## ----------------------------------------------------------------------------
-lcm_2007 <- lcm_2km[, 'new2kid']
-lcm_2007$grass_ha <- lcm_2km[,which(colnames(lcm_2km) == 4), drop=TRUE] * 0.0625
-lcm_2007$sng_ha <- rowSums(lcm_2km[,which(colnames(lcm_2km) %in% c(5:12)), drop=TRUE]) * 0.0625
-lcm_2007$wood_ha <- rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(1:2)), drop=TRUE]) * 0.0625
-lcm_2007$urban_ha <- rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(20:21)), drop=TRUE]) * 0.0625
-lcm_2007$arable_ha <- lcm_2km[, which(colnames(lcm_2km) %in% c(3)), drop=TRUE] * 0.0625
-lcm_2007$freshwater_ha <- lcm_2km[, which(colnames(lcm_2km) %in% c(14)), drop=TRUE] * 0.0625
-lcm_2007$marine_ha <- lcm_2km[, which(colnames(lcm_2km) %in% c(13)), drop=TRUE] * 0.0625
-lcm_2007$ocean_ha <- 0
-lcm_2007$coast_ha <- rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(15:19)), drop=TRUE]) * 0.0625
 
-sum(rowSums(lcm_2007[,3:11, drop=TRUE]) != 400) == 0
+lcm_2007 <- lcm_2km[, 'new2kid']
+lcm_2007$grass_ha <- as.numeric(lcm_2km[,which(colnames(lcm_2km) == 4), drop=TRUE] * 0.0625)
+lcm_2007$sng_ha <- as.numeric(rowSums(lcm_2km[,which(colnames(lcm_2km) %in% c(5:14)), drop=TRUE]) * 0.0625)
+lcm_2007$wood_ha <- as.numeric(rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(1:2)), drop=TRUE]) * 0.0625)
+lcm_2007$urban_ha <- as.numeric(rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(22:23)), drop=TRUE]) * 0.0625)
+lcm_2007$arable_ha <- as.numeric(lcm_2km[, which(colnames(lcm_2km) %in% c(3)), drop=TRUE] * 0.0625)
+lcm_2007$freshwater_ha <- as.numeric(lcm_2km[, which(colnames(lcm_2km) %in% c(16)), drop=TRUE] * 0.0625)
+lcm_2007$marine_ha <- as.numeric(lcm_2km[, which(colnames(lcm_2km) %in% c(15)), drop=TRUE] * 0.0625)
+lcm_2007$coast_ha <- as.numeric(rowSums(lcm_2km[, which(colnames(lcm_2km) %in% c(17:21)), drop=TRUE]) * 0.0625)
+
+# assume ocean to be all empty cells
+uk_lcm_2007 <- lcm_2007 %>% 
+  st_drop_geometry() %>% # remove geometry as it stops the calc
+  mutate(ocean_ha = 400 - (select(., grass_ha:coast_ha) %>% rowSums))# %>% 
+  full_join(seer_2km, by = "new2kid") # add back in geometry 
+
+# check how many 2km cells have ocean 
+uk_lcm_2007 %>% filter(ocean_ha > 0 ) %>% tally() # 2811 2km cells with ocean 
+
+# check all 2km cells = 400 
+sum(rowSums(uk_lcm_2007[,2:10, drop=TRUE]) != 400) == 0 
 
 ## (3) SAVE ON DISK
 ## ================
-setwd('D:/Documents/GitHub/NERC--Agile-Sprint/Data/')
-st_write(lcm_2007, 'baseline_lcm_2007.csv')
+setwd(paste0(path, "/Data/LCM/LCM_2km"))
+st_write(uk_lcm_2007, 'uk_baseline_lu_2007.shp')
+
