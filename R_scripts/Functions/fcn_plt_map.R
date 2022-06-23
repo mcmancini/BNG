d79f121e6c1480370d60ea60ad542e8301cbe1a7
mcmancini <m.c.mancini@exeter.ac.uk>
2022-06-23 10:53:23 +0100

    Added script to run the NEV suite of models
    Added script to run the NEV suite of models to compute change from baseline LCM to urbanisation and from urbanisation baseline to offset scenarios

---
+++ R_scripts/Functions/fcn_plt_map.R
@@ -0,0 +1,191 @@
+## fcn_plt_map.R
+## =============
+##
+## Author: Mattia C. Mancini
+## Created: 16-Mar-2021
+## Last modified: 16-Mar-2021
+## --------------------------
+##
+## Map a spatial sf dataframe, specifying what column of the data to be mapped,
+## how to split the data into classes, the title of the plot and whether to
+## include the legend. 
+## 
+## DETAILS
+## - Data: A spatial sf dataframe
+## - column_name: the exact name of the column in the dataframe to be plotted
+## - classes: 'default' or a specified numeric vector containing the breaks that
+##            separate the wantted classes
+## = plot_title: a character string
+## - plot_legend: can assume values indicating the position of the legend as in 
+##                ggplot theme(legend.position = ....) argument, e.g. 'right', 
+##                'bottom', or 'none'
+## - scale: one of the scale_fill_viridis options
+## =============================================================================
+require(ggplot2)
+require(viridis)
+
+fcn_plt_map <- function(data, 
+                        column_name, 
+                        classes, 
+                        plot_title,
+                        legend_title,
+                        plot_legend, 
+                        scale, 
+                        direction){
+  eng_border <- st_read('D:/Documents/Housing/Data/SEER_GRID/Countries_December_2016_Full_Clipped_Boundaries_in_Great_Britain.shp')
+  # df <- data[, c('new2kid', column_name)]
+  df <- data
+  
+  # legend
+  legend_position <- plot_legend
+  
+  # Set low and high bounds, and split the data into categories for plotting
+  ind <- which(colnames(df) == column_name)
+  low_bound <- min(df[, ind, drop = TRUE], na.rm = TRUE)
+  high_bound <- max(df[, ind, drop = TRUE], na.rm = TRUE)
+  
+  if (is.character(classes) == TRUE){
+    no_classes <- 8
+    quantiles <- seq(low_bound, high_bound, (high_bound - low_bound)/(no_classes - 2))
+  } else {
+    no_classes <- length(classes)
+    quantiles <- classes
+  }
+  
+  labels <- c()
+  for(idx in 1:length(quantiles)){
+    labels <- c(labels, paste0(round(quantiles[idx], digits = 3), 
+                               " - ", round(quantiles[idx + 1], digits = 3)))
+  }
+  labels <- labels[1:length(labels)-1]
+  
+  df$bins <- cut(df[, ind, drop = TRUE], 
+                 breaks = quantiles, 
+                 labels = labels, 
+                 include.lowest = T)
+  
+  plt <- ggplot() +
+    geom_sf(data = eng_border, fill = 'white', lwd = 1) +
+    geom_sf(data = df, 
+            aes(fill = bins), 
+            color = NA) +
+    scale_fill_viridis(
+      option = scale,
+      discrete = T,
+      direction = direction,
+      drop = TRUE,
+      na.value = "white",
+      guide = guide_legend(
+        title =  legend_title,
+        title.position = 'top',
+        reverse = F, 
+        nrow=4,
+        byrow=TRUE)) + 
+    ggtitle(plot_title) +
+    theme(plot.title = element_text(size=30, hjust = 0.5),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),
+          legend.text = element_text(size = 22),
+          legend.title = element_text(size = 24, hjust = 0.5),
+          legend.position = legend_position)
+  
+  #return
+  return(plt)
+}
+
+
+fcn_continuous_plot <- function(plot_data,
+                                country,
+                                column, 
+                                limits, 
+                                plot_title, 
+                                legend_title, 
+                                legend_position, 
+                                scale, 
+                                direction) {
+  df <- plot_data
+  if (country == 'Great Britain'){
+    eng_border <- st_read('D:/Documents/Housing/Data/SEER_GRID/Countries_December_2016_Full_Clipped_Boundaries_in_Great_Britain.shp')
+  } else if (country == 'England'){
+    eng_border <- st_read('D:/Documents/Housing/Data/SEER_GRID/england_full_clipped.shp')
+  } else {
+    stop('Coutries can only be \'England\' or \'Great Britain\'')
+  }
+  
+
+  formatLabel <- function(x) format(round(x, 3), nsmall = 2)
+  formatIntegers <- function(x) format(x, scientific = 999)
+  # labels = parse(text=paste(seq(limits[1], limits[2], ((limits[2] - limits[1]) / 4)), "~ha"))
+  label_text <- paste(seq(limits[1], limits[2], ((limits[2] - limits[1]) / 4)))
+  label_text[5] <- "< 0"
+  labels <- label_text
+  
+  plot <- ggplot() +
+    geom_sf(data = df,
+            aes(fill = get(column), color = get(column))) +
+    scale_fill_viridis(legend_title, 
+                       labels = labels,
+                       option = scale, 
+                       limits = limits, 
+                       discrete = FALSE,
+                       direction = direction) +
+    scale_color_viridis(legend_title, 
+                        labels = labels,
+                        option = scale, 
+                        limits = limits, 
+                        discrete = FALSE,
+                        direction = direction) +
+    geom_sf(data = eng_border, fill = NA, lwd = 1) +
+    ggtitle(plot_title) +
+    theme_bw() +
+    theme(panel.border = element_blank(), 
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank()) +
+    coord_sf(datum = NA) +
+    theme(plot.title = element_text(size=30, hjust = 0.5),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),
+          legend.text = element_text(size = 22),
+          legend.title = element_blank(),
+          legend.position = legend_position, 
+          legend.key.width=unit(1.5,"in"))
+    
+  
+  return(plot)
+}
+
+
+fcn_change_plot <- function(plot_data, 
+                            column, 
+                            limits, 
+                            plot_title, 
+                            legend_title, 
+                            legend_position) {
+  eng_border <- st_read('D:/Documents/Housing/Data/SEER_GRID/Countries_December_2016_Full_Clipped_Boundaries_in_Great_Britain.shp')
+  
+  formatLabel <- function(x) format(round(x, 3), nsmall = 2)
+  formatIntegers <- function(x) format(x, scientific = 999)
+
+  plot <- ggplot() +
+    geom_sf(data = eng_border, fill = 'white', lwd = 1) +
+    geom_sf(data = plot_data,
+            aes(fill = get(column), color = get(column))) +
+    scale_fill_gradient2(low = 'red3',
+                         mid = 'white',
+                         high = 'blue4',
+                         # labels = formatLabel, 
+                         limits = limits) +
+    scale_color_gradient2(low = 'red3',
+                          mid = 'white',
+                          high = 'blue4',
+                          # labels = formatLabel, 
+                          limits = limits) +
+    ggtitle(plot_title) +
+    theme(plot.title = element_text(size=30, hjust = 0.5),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),
+          legend.text = element_text(size = 22),
+          legend.title = element_text(size = 24, hjust = 0.5),
+          legend.position = legend_position)
+  return(plot)
+}
---
+++ run_NEV.m
@@ -0,0 +1,161 @@
+%% run_NEV.m
+%  =========
+% Author: Mattia Mancini, Rebecca Collins
+% Created: 25 Feb 2022
+% Last modified: 22 Jul 2022
+% ---------------------------------------
+% DESCRIPTION
+% Script to run the NEV tool for scenario analysis of changes in ecosystem
+% services as a result to changes in land use. 
+% It requires a baseline land use and a land use that differs from the
+% baseline in order to compute changes in ecosystem services. For the NERC
+% Agile-Sprint the baseline is the land uses from the 2020 land cover map
+% for the BBOWT - NEP area along the Oxford-Cambridge corridor. This has
+% been produced in the script 'calc_baseline_landuses.R'. Scenarios need to
+% be passed as land uses in the same format as the baseline landuse.
+%
+% INPUTS
+%   1) Model parameters
+%   2) flags
+%   3) Land uses
+%      For the models to all work the landuse table must
+%      contain the following columns (the order does NOT matter):
+%      1) new2kid; 2) urban_ha; 3) sngrass_ha; 4) wood_ha; 5) farm_ha; 
+%      6) water_ha. As farm is the sum of arable_ha and grass_ha, if those
+%      are passed, farm_ha is no longer required. The same applies for
+%      water_ha, i.e. the sum of freshwater_ha, marine_ha, coast_ha and
+%      ocean_ha.
+%      NB: more columns can be passed, to override specific default
+%      landuses:
+%      - wood_mgmt_ha can be passed to override the default values in the
+%        NEV database. The default proportions of coniferous and
+%        deciduous woodland will still be applied, as well as the
+%        proportions for non managed woodland. 
+%         -------   AAA   ------ 
+%        if new woodland is created in a scenario, this must be managed for 
+%        the correct calculation of GHGs. If wood_mgmt_ha is passed in the 
+%        scenario land use, but new woodland from baseline in the scenario 
+%        is more than the woodland in wood_mgmt_ha (i.e. 
+%        wood_mgmt_ha(scenario) < (wood_ha(scenario) - wood_ha(baseline))
+%        then the script will overwrtie the wood_mgmt_ha passed and replace 
+%        it with the hectares of new woodland (i.e. wood_mgmt_ha(scenario) 
+%        = (wood_ha(scenario) - wood_ha(baseline))
+%      - it is possible to override the top-level farm model that allocates
+%        land between arable and farm grassland, passing values of grass_ha
+%        (farm grassland) and arable_ha. In this case the farm model will
+%        still allocate arable between the various crop types and grassland
+%        between grassland types. To do this, grass_ha and arable_ha values
+%        must be passed, and the model_flags.run_ag_toplevel must be set to
+%        false. 
+%
+%        !!!!!!!!!  AAA  !!!!!!!!
+%        scenario land uses assume that the land use change
+%        passed happens in YEAR 1 !!!!!!!
+% =========================================================================
+
+%% (0) Set up
+%      (a) model parameters and selection of modules to run
+%  ========================================================
+clear
+addpath(genpath('D:\Documents\GitHub\Run_NEV_Scenarios\'));
+
+% 1.1. model parameters related to land uses and farm model
+% ---------------------------------------------------------
+parameters = fcn_set_parameters();
+parameter.parent_dir       = 'D:\Documents\NEV\Model Data\';
+parameters.lcm_data_folder = 'D:\Documents\GitHub\BNG\Data\LCM\LCM_2km\';
+parameters.num_years                   = 40;
+parameters.start_year                  = 2020;
+parameters.clim_string                 = 'ukcp18';
+parameters.clim_scen_string            = 'rcp60';
+parameters.temp_pct_string             = '50';
+parameters.rain_pct_string             = '50';
+parameters.biodiversity_climate_string = 'future';
+parameters.other_ha                    = 'baseline'; 
+parameters.landuse_change_timeframe    = 50; % land use change remains for these numbers of years
+
+% 1.2. Model parameters for valuation
+% -----------------------------------
+parameters.assumption_flooding = 'low';
+parameters.assumption_nonuse = 0.38; % this could be 0.38, 0.75, 1 
+parameters.assumption_pop = 'low';
+
+% 1.3. Land use changes allowed
+% -----------------------------
+parameters.options = {'arable2sng', 'arable2wood', 'arable2urban', 'arable2mixed', ...
+                      'grass2sng', 'grass2wood', 'grass2urban', 'grass2mixed', ...
+                      'sng2urban', 'wood2urban'};
+
+
+% 1.3. Flags to select which models to run
+% ----------------------------------------
+model_flags.run_ag_toplevel   = true;
+model_flags.run_ghg           = true;
+model_flags.run_forestry      = true;
+model_flags.run_biodiversity  = true;
+model_flags.run_hydrology     = true;
+model_flags.run_recreation    = true;
+
+
+
+%% (2) LOAD LAND USES
+%      2.1 - Load baseline land use data
+%      2.2 - Run the scenario land use data
+%  ========================================
+
+% 2.1. Load baseline land uses. This is either a land cover map from CEH,
+%      or a modification of one of the CEH LCMs. When passing a land cover
+%      table, we also need to specify which CEH LCM it originates from in
+%      order to correclty calculate baselines for each of the NEV modules.
+% ------------------------------------------------------------------------
+base_ceh_lcm = '2007';
+baseline_lu = readtable(strcat(parameters.lcm_data_folder, 'lcm_aggr_2007.csv'));
+parameters.base_ceh_lcm = base_ceh_lcm;
+
+baseline_lu.farm_ha = baseline_lu.arable_ha + baseline_lu.grass_ha;
+baseline_lu.arable_ha = [];
+baseline_lu.grass_ha = [];
+
+
+% server_flag = false;
+% conn = fcn_connect_database(server_flag);
+% sqlquery = ['SELECT ', ...
+%                     'new2kid, ', ...
+%                     'farm_ha, ', ...
+%                     'water_ha, ', ...
+%                     'urban_ha, ', ...
+%                     'sngrass_ha, ', ...
+%                     'wood_ha ,', ...
+%                     'wood_mgmt_ha ' ...
+%                 'FROM nevo.nevo_variables ORDER BY new2kid'];
+% setdbprefs('DataReturnFormat','table');
+% dataReturn  = fetch(exec(conn,sqlquery));
+% baseline_lu = dataReturn.Data;
+% baseline_lu.Properties.VariableNames{5} = 'sng_ha';
+
+
+% 2.2. Load scenario land use
+% ---------------------------
+landuse_data_path = 'D:\Documents\GitHub\BNG\Data\Urban Sprawl - F.Eigenbrod\';
+scenario_lu = readtable(strcat(landuse_data_path, 'urban_sprawl_2km_farm.csv'));
+scenario_lu = scenario_lu(:, 1:6);
+scenario_lu{:, 2:6} = scenario_lu{:, 2:6} .* 400;
+scenario_lu.Properties.VariableNames = {'new2kid', 'water_ha', ...
+                                        'urban_ha', 'sng_ha', 'wood_ha',...
+                                        'farm_ha'};
+                                    
+% The baseline LCM is for the whole of the UK, whereas the
+% urbanisation model only for England and Wales. Reduce LCM to EW
+[~, idx] = intersect(baseline_lu.new2kid, scenario_lu.new2kid);
+baseline_lu = baseline_lu(idx, :);
+
+
+%% (3) RUN THE MODELS
+%  ==================
+[benefits, costs, env_outs, es_outs] = fcn_run_scenario(model_flags, ...
+                            parameters, ...
+                            baseline_lu, ... 
+                            scenario_lu);
+
+
+
